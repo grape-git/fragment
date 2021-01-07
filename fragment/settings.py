5346625500769f126a3057493ba67341b13e4b84
jodong <ehdrms5665@naver.com>
2021-01-07 18:00:35 +0900

    djust Django course 8 modification

--- fragment/settings.py
+++ fragment/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'fragment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'fragment', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
